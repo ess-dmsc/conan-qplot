80db2c98ca4ec1a4744ce5ad9be56423bc0a7689
Martin Shetty <martukas@gmail.com>
2018-06-14 01:26:26 +0200

    test app no longer trivial

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,15 +1,11 @@
 #include <QApplication>
-//#include "QtColorWidgets/color_selector.hpp"
+#include "QPlot2D.h"
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
 
-  //color_widgets::ColorSelector cs;
+  QPlot::MarkerBox2D box;
 
-//  TestApp w;
-//  w.show();
-
-//  return a.exec();
   return EXIT_SUCCESS;
 }
