f8553cbfd01acbdec4e0df61afa2fc2c630f86c5
Mads Ipsen <mads.ipsen@gmail.com>
2025-02-14 20:21:01 +0100

    - Testing

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -20,7 +20,7 @@ class qplotTestConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
-            # self.run("ldd `which qmake6`")
+            self.run("ldd `which qmake6`")
             # self.run("strip -v --remove-section=.note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt6*.so.?.?.?")
             # self.run("ldd `which qmake6`")
             self.run(".%sexample -platform offscreen" % os.sep)
