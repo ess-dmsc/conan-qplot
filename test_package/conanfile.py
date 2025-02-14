3d40a54646112991773c4405cf06f48a52e9a4b8
Mads Ipsen <mads.ipsen@gmail.com>
2025-02-14 19:39:36 +0100

    - Testing

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -20,4 +20,5 @@ class qplotTestConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
+            self.run("uname -a")
             self.run(".%sexample -platform offscreen" % os.sep, ignore_errors=True)
