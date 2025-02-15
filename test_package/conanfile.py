f8891298d1148b8d29327cac4da37f675781461a
Mads Ipsen <mads.ipsen@gmail.com>
2025-02-15 07:05:52 +0100

    - Test

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -18,7 +18,7 @@ class qplotTestConan(ConanFile):
         self.copy("*.a", dst="bin", src='lib')
 
     def test(self):
-        # Test
+        # Test not
         if not tools.cross_building(self.settings):
             os.chdir("bin")
 #            self.run("objdump -s -j .note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt6Core.so.6.2.4")
