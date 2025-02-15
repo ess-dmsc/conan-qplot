0f6bd4f5063ff04bc8d51c67b402db84cc779bdd
Mads Ipsen <mads.ipsen@gmail.com>
2025-02-15 07:04:40 +0100

    - Test

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -23,6 +23,4 @@ class qplotTestConan(ConanFile):
             os.chdir("bin")
 #            self.run("objdump -s -j .note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt6Core.so.6.2.4")
             self.run("ldd `which qmake6`")
-            self.run("strip -v --remove-section=.note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt6*.so.?.?.?")
-            self.run("ldd `which qmake6`")
             self.run(".%sexample -platform offscreen" % os.sep)
